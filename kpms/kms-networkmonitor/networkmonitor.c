c6b66a9aae04e953eb2610a94fb51285f42a9de1
FantasySR <192044614@qq.com>
2026-05-02 16:16:12 +0800

    更新 networkmonitor.c

--- kpms/kms-networkmonitor/networkmonitor.c
+++ kpms/kms-networkmonitor/networkmonitor.c
@@ -5,41 +5,28 @@
 #include <syscall.h>
 
 KPM_NAME("KMS_NetMonitor");
-KPM_VERSION("2.0.1");
+KPM_VERSION("2.0.2");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("FantasySR");
-KPM_DESCRIPTION("Network monitor skeleton with openat");
+KPM_DESCRIPTION("Single connect hook test");
 
-static int monitor_running = 0;
-
-// 用已验证成功的 openat 作为骨架
-static void before_openat(hook_fargs4_t *fargs, void *udata) {
-    if (!monitor_running) return;
-    printk(KERN_INFO "KMS_NET| OPENAT called\n");
-}
-
-static long control0(const char *args, char *__user out_msg, int outlen) {
-    if (!args) { if (out_msg) strncpy(out_msg, "no cmd", outlen); return 0; }
-    if (strcmp(args, "run") == 0) { monitor_running=1; printk(KERN_INFO "KMS_NET: running\n"); if (out_msg) strncpy(out_msg, "running", outlen); }
-    else if (strcmp(args, "stop") == 0) { monitor_running=0; printk(KERN_INFO "KMS_NET: stopped\n"); if (out_msg) strncpy(out_msg, "stopped", outlen); }
-    else { if (out_msg) strncpy(out_msg, "unknown", outlen); }
-    return 0;
+static void before_connect(hook_fargs3_t *fargs, void *udata) {
+    printk(KERN_INFO "KMS_NET| CONNECT called\n");
 }
 
 static long init(const char *args, const char *event, void *__user reserved) {
     printk(KERN_INFO "KMS_NET: init start\n");
-    hook_err_t err = fp_hook_syscalln(__NR_openat, 4, before_openat, 0, 0);
-    printk(KERN_INFO "KMS_NET: openat hook err=%d\n", err);
+    hook_err_t err = fp_hook_syscalln(__NR_connect, 3, before_connect, 0, 0);
+    printk(KERN_INFO "KMS_NET: connect hook err=%d\n", err);
     printk(KERN_INFO "KMS_NET: init done\n");
     return 0;
 }
 
 static long netmon_exit(void *__user reserved) {
-    fp_unhook_syscalln(__NR_openat, before_openat, 0);
+    fp_unhook_syscalln(__NR_connect, before_connect, 0);
     printk(KERN_INFO "KMS_NET: unloaded\n");
     return 0;
 }
 
 KPM_INIT(init);
 KPM_EXIT(netmon_exit);
-KPM_CTL0(control0);
